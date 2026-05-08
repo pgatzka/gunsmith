bcff9ae8d729dcebb2d73286e4cd27d3806cccd1
Philipp Gatzka <phil.gatzka@gmail.com>
2026-05-08 18:33:13 +0200

    Move everything to stash folder

--- src/main/java/io/github/pgatzka/gunsmith/batch/pojo/BuildResult.java
+++
@@ -1,7 +0,0 @@
-package io.github.pgatzka.gunsmith.batch.pojo;
-
-import io.github.pgatzka.gunsmith.data.pojo.Build;
-
-public record BuildResult(Long weaponId, double ergonomics, double recoilHorizontal, double recoilVertical, Build build) {
-
-}
--- src/main/java/io/github/pgatzka/gunsmith/batch/pojo/BuildSettings.java
+++
@@ -1,4 +0,0 @@
-package io.github.pgatzka.gunsmith.batch.pojo;
-
-public record BuildSettings(String id) {
-}
